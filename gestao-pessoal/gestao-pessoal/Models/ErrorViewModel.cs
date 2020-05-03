6225a00af38f3f5e5852f5095bbede9ff6232b5e
Carlos Germano <germano.carlos2712@gmail.com>
2020-05-03 12:07:56 -0300

    First Commit

---
+++ gestao-pessoal/gestao-pessoal/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace gestao_pessoal.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
