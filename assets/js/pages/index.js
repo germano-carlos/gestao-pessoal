083b19d8df3a4cd4d56a56262686cabf285d0f84
Carlos <germano.carlos2712@gmail.com>
2020-07-04 19:10:45 -0300

    Register Page

--- assets/js/pages/index.js
+++ assets/js/pages/index.js
@@ -5,6 +5,46 @@ function submitFunction(event) {
 
     const inputEmail = document.querySelector('input[name=username]').value;
     const inputSenha = document.querySelector('input[name=userpassword]').value;
+    var userRegister = JSON.parse(localStorage.getItem("register"));
+
+    for(j=0;j<userRegister.length;j++)
+    {
+        if (inputEmail == userRegister[j].email && inputSenha == userRegister[j].senha) 
+        {
+            let task1 = [];
+            check = true;
+            
+            localStorage.setItem("id",userRegister[j].id)
+            localStorage.setItem("email",userRegister[j].email)
+            localStorage.setItem("nome",userRegister[j].nome)
+            localStorage.setItem("dependentes",userRegister[j].dependentes)
+            localStorage.setItem("sexo",userRegister[j].sexo)
+            
+            for(k = 0; k < db.tasksdata.length; k++)
+            {
+                if((userRegister[j].id == db.tasksdata[k].responsavel_id))
+                {
+                    task1.push(db.tasksdata[k]);
+                }
+                else if (db.tasksdata[k].possui_dependente)
+                {
+                    for(j = 0; j < userRegister[j].dependentes.length; j++)
+                    {
+                        if(userRegister[j].dependentes[j] == db.tasksdata[k].responsavel_id)
+                        {
+                            task1.push(db.tasksdata[k]);
+                        }
+                    }
+                }
+            }
+
+            localStorage.setItem("tasks", JSON.stringify(task1));
+            localStorage.setItem("users", JSON.stringify(db.customerdata));
+            alert("Usuario encontrado, você será redirecionado");
+            window.location.replace("home.html")
+        }
+    }
+
     for (i = 0; i < db.customerdata.length; i++) {
             if (inputEmail == db.customerdata[i].email && inputSenha == db.customerdata[i].senha) 
             {
